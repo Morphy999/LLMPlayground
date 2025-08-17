75a0ce402ac9ad182fbf00631d1e4e029583de08
Morphy999 <walterbuenoneto8575@gmail.com>
2025-08-16 23:26:19 -0300

    Add plot_losses to utils exports for enhanced metrics visualization

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -4,10 +4,12 @@ Utilitários gerais do LLM Playground
 
 from .config import GPTConfig, get_model_config
 from .logging import get_logger, setup_logging
+from .metrics import plot_losses
 
 __all__ = [
     "GPTConfig",
     "get_model_config",
     "setup_logging",
     "get_logger",
+    "plot_losses",
 ]
